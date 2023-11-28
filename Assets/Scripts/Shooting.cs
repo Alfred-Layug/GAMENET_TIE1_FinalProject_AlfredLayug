b166692d1635e3d868e6c16c6e78c7a5fc93840a
Alfred Angelo Layug <alfredangelo.layug@benilde.edu.ph>
2023-11-29 03:34:48 +0800

    Code Polishing

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -14,13 +14,11 @@ public class Shooting : MonoBehaviourPunCallbacks
     public TextMeshProUGUI _playerNameText;
 
     private Animator _animator;
-    private int _score;
 
     private void Start()
     {
         _playerNameText.text = photonView.Owner.NickName;
         _animator = this.GetComponent<Animator>();
-        _score = 0;
     }
 
     private void Update()
@@ -57,68 +55,5 @@ public class Shooting : MonoBehaviourPunCallbacks
     {
         GameObject hitEffectGameObject = Instantiate(_bulletEffectPrefab, position, Quaternion.identity);
         Destroy(hitEffectGameObject, 0.2f);
-    }
-
-    public void Die()
-    {
-        if (photonView.IsMine)
-        {
-            _animator.SetBool("isDead", true);
-        }
-    }
-
-    [PunRPC]
-    public void ShowKillFeed(string feedText)
-    {
-        GameObject killFeedText = GameObject.Find("Kill Feed Text");
-        killFeedText.GetComponent<TextMeshProUGUI>().text = feedText;
-    }
-
-    [PunRPC]
-    public void HideKillFeed()
-    {
-        GameObject killFeedText = GameObject.Find("Kill Feed Text");
-        killFeedText.GetComponent<TextMeshProUGUI>().text = "";
-    }
-
-    private void UpdateKillAmount()
-    {
-        if (photonView.IsMine)
-        {
-            _score++;
-            GameObject killsText = GameObject.Find("Kills Text");
-            killsText.GetComponent<TextMeshProUGUI>().text = "Kills: " + _score;
-
-            if (_score >= 10)
-            {
-                photonView.RPC("ShowWinner", RpcTarget.All, photonView.Owner.NickName);
-            }
-        }
-    }
-
-    [PunRPC]
-    public void ShowWinner(string winnerName)
-    {
-        GameObject winnerText = GameObject.Find("Winner Text");
-        
-        if (winnerText.GetComponent<TextMeshProUGUI>().text == "")
-        {
-            winnerText.GetComponent<TextMeshProUGUI>().text = winnerName + " is the winner!";
-        }
-
-        StartCoroutine(ReturnToLobbyTimer());
-    }
-
-    [PunRPC]
-    public void ReturnToLobby()
-    {
-        PhotonNetwork.LeaveRoom();
-        PhotonNetwork.LoadLevel("LobbyScene");
-    }
-
-    private IEnumerator ReturnToLobbyTimer()
-    {
-        yield return new WaitForSeconds(4);
-        photonView.RPC("ReturnToLobby", RpcTarget.All);
     }
 }
